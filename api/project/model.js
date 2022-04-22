3e6496c8725d37b60fc3cf6ddf4be9bcf9cc4a99
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-04-22 09:04:01 -0700

    server up and running

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,30 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig');
+
+async function getAllProjects() {
+    const projects = await db('projects');
+    project.forEach(project => {
+        project.project_completed = !!project.project_completed;
+        return projects;
+    })
+}
+
+async function getProjectById(id) {
+    const [project] = await db('projects')
+        .where('project_id', id);
+
+    if(project) {
+        project.project_complete = !!project.project_completed;
+    }
+    return project;
+}
+
+async function createProject(project) {
+    const [id] = await db('projects')
+        .insert(project);
+
+    const created = await getProjectById(id);
+    return created;
+}
+
+module.exports = { getAllProjects, getProjectById, createProject };
--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,23 @@
 // build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+async function getAllResources() {
+    const resources = await db('resources');
+    return resources;
+}
+
+async function getResourceById(id) {
+    const [resource] = await db('resources')
+        .where('resource_id', id);
+    return resource;
+}
+
+async function createResource(resource) {
+    const [id] = await db('resources')
+        .insert(resource);
+
+    const created = await getResourceById(id);
+    return created;
+}
+
+module.exports = { getAllResources, getResourceById, createResource };
