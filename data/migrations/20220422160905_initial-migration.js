0eca9384b051b5599d7f62756e1aef5944423457
Rlauckern <90931921+Rlauckern@users.noreply.github.com>
2022-04-22 09:15:02 -0700

    migration.js up and down

--- data/migrations/20220422160905_initial-migration.js
+++ data/migrations/20220422160905_initial-migration.js
@@ -1,15 +1,59 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+exports.up = function(knex) {
+    return knex.schema
+      .createTable('projects', tbl => {
+          tbl.increments('project_id');
+          tbl.string('project_name', 200)
+              .notNullable();
+          tbl.string('project_description', 300);
+          tbl.boolean('project_completed')
+              .defaultTo(0);
+      })
+      .createTable('resources', tbl => {
+          tbl.increments('resource_id');
+          tbl.string('resource_name', 200)
+              .notNullable()
+              .unique();
+          tbl.string('resource_description', 300);
+      })
+      .createTable('tasks', tbl => {
+          tbl.increments('task_id');
+          tbl.string('task_description', 200)
+              .notNullable();
+          tbl.string('task_notes', 300);
+          tbl.boolean('task_completed')
+              .defaultTo(0);
+          tbl.integer('project_id')
+              .unsigned()
+              .notNullable()
+              .references('project_id')
+              .inTable('projects')
+              .onDelete('RESTRICT')
+              .onUpdate('RESTRICT');
+      })
+      .createTable('project_resources', tbl => {
+          tbl.increments('project_resource_id');
+          tbl.integer('project_id')
+              .unsigned()
+              .notNullable()
+              .references('project_id')
+              .inTable('projects')
+              .onDelete('RESTRICT')
+              .onUpdate('RESTRICT');
+          tbl.integer('resource_id')
+              .unsigned()
+              .notNullable()
+              .references('resource_id')
+              .inTable('resources')
+              .onDelete('RESTRICT')
+              .onUpdate('RESTRICT');
+      })
+  };
   
-};
+  exports.down = function(knex) {
+    return knex.schema
+      .dropTableIfExists('project_resources')
+      .dropTableIfExists('tasks')
+      .dropTableIfExists('resources')
+      .dropTableIfExists('projects')
+  };
